e360b143e6979e2c443aeda8a91346971a77ff71
yaseenssenyonjo <yaseenssenyonjo@gmail.com>
2019-05-03 23:39:14 +0100

    Removal of old code.

--- Interpreted-Language/Language/Interpreter/Traits/IExecutionContext.cs
+++
@@ -1,9 +0,0 @@
-namespace Interpreted_Language.Language.Interpreter.Traits
-{
-    /// <summary>
-    /// Represents the context in which nodes are executing in.
-    /// </summary>
-    internal interface IExecutionContext
-    {
-    }
-}
