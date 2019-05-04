20a3cc072656374dbb6a73923738e5bbf883492b
yaseenssenyonjo <yaseenssenyonjo@gmail.com>
2019-05-04 22:35:34 +0100

    Wrote code to test the functionality of the parser.

--- Interpreted-Language/Program.cs
+++ Interpreted-Language/Program.cs
@@ -1,8 +1,29 @@
 using System;
+using InterpretedLanguage.Lexer;
+using InterpretedLanguage.Parser;
+using InterpretedLanguage.Parser.Groups.Statements;
+using InterpretedLanguage.Parser.SyntaxTree;
+using InterpretedLanguage.Parser.SyntaxTree.Traits;
 using InterpretedLanguage.Tokens;
 
 namespace InterpretedLanguage
 {
+    internal class DummyNode : INode
+    {
+        private readonly string _name;
+        
+        public DummyNode(string name)
+        {
+            _name = name;
+        }
+        
+        public AdvancementType Execute(SyntaxTree tree)
+        {
+            Console.WriteLine(_name);
+            return AdvancementType.Continue;
+        }
+    }
+    
     internal class Program
     {
         public static void Main(string[] args)
@@ -11,11 +32,23 @@ namespace InterpretedLanguage
             lexer.CreateGrammar(string.Empty)
                 .AddRule(TokenType.Identifier, "(.+)");
             var tokens = lexer.Tokenise("hey");
-
+            
             foreach (var token in tokens)
             {
                 Console.WriteLine(token);
             }
+            
+            var parser = new Parser.Parser();
+            
+            parser.CreateGroup("New Lines")
+                .Add(new ExpectAndIgnore(TokenType.NewLine));
+            
+            parser.CreateGroup(string.Empty)
+                .Add(new Capture(TokenType.Identifier, "name"))
+                .CreateNode((variables, lineNumber) => new DummyNode((string)variables["name"]));
+            
+            var tree = new SyntaxTree();
+            parser.Parse(tree, tokens);
         }
     }
 }
