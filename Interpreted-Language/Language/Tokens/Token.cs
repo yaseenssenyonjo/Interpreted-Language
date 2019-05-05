b95b0d333824a12f3cef16df75eb7f11c636fa20
yaseenssenyonjo <yaseenssenyonjo@gmail.com>
2019-05-06 00:40:00 +0100

    Documented tokens.

--- Interpreted-Language/Language/Tokens/Token.cs
+++ Interpreted-Language/Language/Tokens/Token.cs
@@ -1,11 +1,29 @@
 namespace InterpretedLanguage.Language.Tokens
 {
+    /// <summary>
+    /// Represents a token.
+    /// </summary>
     internal class Token
     {
-        public readonly int LineNumber;
+        /// <summary>
+        /// The type of token.
+        /// </summary>
         public readonly int Type;
+        /// <summary>
+        /// The value of the token.
+        /// </summary>
         public readonly object Value;
+        /// <summary>
+        /// The line number of the token.
+        /// </summary>
+        public readonly int LineNumber;
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Token"/> class.
+        /// </summary>
+        /// <param name="type">The type of token.</param>
+        /// <param name="value">The value of the token.</param>
+        /// <param name="lineNumber">The line number of the token.</param>
         public Token(int type, object value, int lineNumber)
         {
             Type = type;
