b43d3b57252c4764dd13004bb421d399f757425e
yaseenssenyonjo <yaseenssenyonjo@gmail.com>
2019-05-06 00:57:21 +0100

    Documented the parser.

--- Interpreted-Language/Language/Parser/SyntaxTree/SyntaxTree.cs
+++ Interpreted-Language/Language/Parser/SyntaxTree/SyntaxTree.cs
@@ -3,35 +3,74 @@ using InterpretedLanguage.Language.Parser.SyntaxTree.Traits;
 
 namespace InterpretedLanguage.Language.Parser.SyntaxTree
 {
+    /// <summary>
+    /// Represents a syntax tree.
+    /// </summary>
     internal class SyntaxTree
     {
+        /// <summary>
+        /// The nodes.
+        /// </summary>
         private readonly List<INode> _nodes = new List<INode>();
+        /// <summary>
+        /// The current index.
+        /// </summary>
         private int _index;
-
+        
+        /// <summary>
+        /// The environment for this syntax tree.
+        /// </summary>
+        public SyntaxTreeEnvironment Environment { get; }
+        
+        /// <summary>
+        /// Initialises a new instance of the <see cref="SyntaxTree"/> class.
+        /// </summary>
         public SyntaxTree()
         {
             Environment = null;
         }
-
+        
+        /// <summary>
+        /// Initialises a new instance of the <see cref="SyntaxTree"/> class with the specified environment.
+        /// </summary>
+        /// <param name="environment"></param>
         public SyntaxTree(SyntaxTreeEnvironment environment)
         {
             Environment = environment;
         }
-
-        public SyntaxTreeEnvironment Environment { get; }
-
+        
+        /// <summary>
+        /// Adds the specified node.
+        /// </summary>
+        /// <param name="node">The node.</param>
         public void Add(INode node)
         {
             _nodes.Add(node);
         }
-
+        
+        /// <summary>
+        /// Advances until the next halting node. 
+        /// </summary>
+        /// <returns>The state of the syntax tree.</returns>
         public SyntaxTreeState Advance()
         {
             while (_index < _nodes.Count)
+            {
                 if (_nodes[_index++].Execute(this) == AdvancementType.Halt)
+                {
                     return SyntaxTreeState.Incomplete;
+                }
+            }
 
             return SyntaxTreeState.Completed;
+        }
+        
+        /// <summary>
+        /// Resets the syntax tree.
+        /// </summary>
+        public void Reset()
+        {
+            _index = 0;
         }
     }
 }
--- Interpreted-Language/Language/Parser/SyntaxTree/SyntaxTreeState.cs
+++ Interpreted-Language/Language/Parser/SyntaxTree/SyntaxTreeState.cs
@@ -1,8 +1,17 @@
 namespace InterpretedLanguage.Language.Parser.SyntaxTree
 {
+    /// <summary>
+    /// Specifies the state of the syntax tree.
+    /// </summary>
     internal enum SyntaxTreeState
     {
+        /// <summary>
+        /// Specifies the syntax tree has not completed execution.
+        /// </summary>
         Incomplete,
+        /// <summary>
+        /// Specifies the syntax tree has completed execution.
+        /// </summary>
         Completed
     }
 }
