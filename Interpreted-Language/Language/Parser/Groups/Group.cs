0147b0b716cc5d15c62c3f886191e16cd5c355c9
yaseenssenyonjo <yaseenssenyonjo@gmail.com>
2019-05-06 17:16:05 +0100

    Completed documentation for the parser.

--- Interpreted-Language/Language/Parser/Groups/Group.cs
+++ Interpreted-Language/Language/Parser/Groups/Group.cs
@@ -6,35 +6,72 @@ using InterpretedLanguage.Language.Tokens;
 
 namespace InterpretedLanguage.Language.Parser.Groups
 {
+    /// <summary>
+    /// Represents a group.
+    /// </summary>
     internal class Group
     {
+        /// <summary>
+        /// The statements.
+        /// </summary>
         private readonly List<IGroupStatement> _statements = new List<IGroupStatement>();
+        /// <summary>
+        /// The variables.
+        /// </summary>
         private readonly Dictionary<string, object> _variables = new Dictionary<string, object>();
 
+        /// <summary>
+        /// The node create function.
+        /// </summary>
         private Func<Dictionary<string, object>, int, INode> _createNodeFunc;
 
+        /// <summary>
+        /// Adds the specified statement.
+        /// </summary>
+        /// <param name="statement">The statement.</param>
+        /// <returns>This instance.</returns>
         public Group Add(IGroupStatement statement)
         {
             _statements.Add(statement);
             return this;
         }
 
+        /// <summary>
+        /// Adds the specified pattern.
+        /// </summary>
+        /// <param name="pattern">The pattern.</param>
+        /// <returns>This instance.</returns>
         public Group Add(IGroupPattern pattern)
         {
-            pattern.CreateStatements().ForEach(_statements.Add);
+            pattern.GetStatements().ForEach(_statements.Add);
             return this;
         }
 
+        /// <summary>
+        /// Adds a variable with the specified name and value.
+        /// </summary>
+        /// <param name="name">The name of the variable.</param>
+        /// <param name="value">The value of the variable.</param>
         public void AddVariable(string name, object value)
         {
             _variables.Add(name, value);
         }
 
-        public void CreateNode(Func<Dictionary<string, object>, int, INode> createNodeFunc)
+        /// <summary>
+        /// Sets the node create function.
+        /// </summary>
+        /// <param name="func"></param>
+        public void CreateNode(Func<Dictionary<string, object>, int, INode> func)
         {
-            _createNodeFunc = createNodeFunc;
+            _createNodeFunc = func;
         }
 
+        /// <summary>
+        /// Matches the specified tokens.
+        /// </summary>
+        /// <param name="tree">The abstract syntax tree.</param>
+        /// <param name="tokens">The tokens.</param>
+        /// <returns>true, if this group matches the tokens; otherwise, false</returns>
         public bool Matches(SyntaxTree.SyntaxTree tree, TokenList tokens)
         {
             _variables.Clear();
--- Interpreted-Language/Language/Parser/Groups/Traits/IGroupPattern.cs
+++ Interpreted-Language/Language/Parser/Groups/Traits/IGroupPattern.cs
@@ -2,8 +2,15 @@ using System.Collections.Generic;
 
 namespace InterpretedLanguage.Language.Parser.Groups.Traits
 {
+    /// <summary>
+    /// Represents statements in a certain order.
+    /// </summary>
     internal interface IGroupPattern
     {
-        List<IGroupStatement> CreateStatements();
+        /// <summary>
+        /// Gets a list of statements.
+        /// </summary>
+        /// <returns>A list of statements</returns>
+        List<IGroupStatement> GetStatements();
     }
 }
