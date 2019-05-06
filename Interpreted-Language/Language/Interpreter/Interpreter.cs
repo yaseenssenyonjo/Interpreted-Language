07f4e6b42f8e9d0ee7345becc52fce4152670c37
yaseenssenyonjo <yaseenssenyonjo@gmail.com>
2019-05-06 17:13:18 +0100

    Documented the interpreter.

--- Interpreted-Language/Language/Interpreter/Interpreter.cs
+++ Interpreted-Language/Language/Interpreter/Interpreter.cs
@@ -4,21 +4,40 @@ using InterpretedLanguage.Language.Parser.SyntaxTree;
 
 namespace InterpretedLanguage.Language.Interpreter
 {
+    /// <summary>
+    /// Represents an interpreter.
+    /// </summary>
     internal class Interpreter
     {
+        /// <summary>
+        /// The execution stack.
+        /// </summary>
         private readonly Stack<SyntaxTree> _executionStack = new Stack<SyntaxTree>();
 
+        /// <summary>
+        /// Pushes the specified tree to the bottom of the execution stack.
+        /// </summary>
+        /// <param name="tree">The abstract syntax tree.</param>
         public void PushRoot(SyntaxTree tree)
         {
             if (_executionStack.Count > 0) _executionStack.Clear();
             _executionStack.Push(tree);
         }
 
+        /// <summary>
+        /// Pushes the specified tree to the top of the execution stack.
+        /// </summary>
+        /// <param name="tree">The abstract syntax tree.</param>
         public void Push(SyntaxTree tree)
         {
             _executionStack.Push(tree);
         }
 
+        /// <summary>
+        /// Advances through the tree.
+        /// </summary>
+        /// <returns>true, if execution is not completed; otherwise, false.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public bool Advance()
         {
             while (true)
@@ -32,6 +51,7 @@ namespace InterpretedLanguage.Language.Interpreter
                         return true;
 
                     case SyntaxTreeState.Completed:
+                        // Preserve the root tree in the stack.
                         if (_executionStack.Count > 1)
                         {
                             _executionStack.Pop();
