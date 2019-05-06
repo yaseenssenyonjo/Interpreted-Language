7fd7fb6f6faa3ec64e8aba6388a47301bac67b23
yaseenssenyonjo <yaseenssenyonjo@gmail.com>
2019-05-06 17:16:05 +0100

    Completed documentation for the parser.

--- Interpreted-Language/Language/Parser/Groups/Traits/IGroupStatement.cs
+++ Interpreted-Language/Language/Parser/Groups/Traits/IGroupStatement.cs
@@ -2,8 +2,17 @@ using InterpretedLanguage.Language.Tokens;
 
 namespace InterpretedLanguage.Language.Parser.Groups.Traits
 {
+    /// <summary>
+    /// Represents a statement.
+    /// </summary>
     internal interface IGroupStatement
     {
+        /// <summary>
+        /// Matches the specified tokens.
+        /// </summary>
+        /// <param name="group">The group for this statement.</param>
+        /// <param name="tokens">The tokens.</param>
+        /// <returns>true, if this statements matches the tokens; otherwise, false</returns>
         bool Match(Group group, TokenList tokens);
     }
 }
